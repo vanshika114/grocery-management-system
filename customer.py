6615fda8fb90dd986a4457d09f178be19cbb1341
vanshika114 <vs0411007@gmail.com>
2026-01-11 15:01:28 +0530

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,20 +1,30 @@
-# --- add items in cart ----
-def add_items():
-    print("inventory",prod)
-    cart = {}
-    while True:
-        item = input("Enter item to be added in cart: ")
-        qty = int(input("Enter quantity of item: "))
-        if qty > prod[item][1]:
-            cart[item] = [prod[item][0],prod[item][1]] 
-        else: 
-            cart[item] = [prod[item][0],qty]
-        ch = input("Continue? Y/N ")
-        if ch.lower() == 'n':
-            break
-    print("cart", cart)
+import admin
 
-# ---- delete item from cart ---- 
+cart = {}
+
+# add products in cart 
+def add_items():
+    if not admin.prod:
+        print("No products available")
+        return
+
+    print("Inventory:", admin.prod)
+
+    while True:
+        item = input("Enter item to add: ").lower()
+
+        if item not in admin.prod:
+            print("Item not found")
+            continue
+
+        qty = int(input("Enter quantity: "))
+        cart[item] = [admin.prod[item][0], qty]
+
+        ch = input("Continue? (Y/N): ").lower()
+        if ch == 'n':
+            break
+
+# delete product from cart 
 def delete_item():
     print("cart ---> ", cart)
     while True:
@@ -29,8 +39,7 @@ def delete_item():
             break
     print("Updated Cart --->", cart)
 
-
-# ---- update item quantity ---- 
+# update product quantity 
 def update_item_qty():
     print("cart ---> ", cart)
     item = input("Enter product name: ")
@@ -41,12 +50,41 @@ def update_item_qty():
         cart[item][1] = qty 
     print("Updated Cart ----> ", cart)
 
-# ---- view total price ---- 
-def view_tp():
-    print("cart ---->", cart)
-    l = []
-    for item in cart:
-        val = (cart[item][0] * cart[item][1])
-        l.append(val)
-    tp = sum(l)
-    print("Total Price = ", tp)
+
+# view total price of cart 
+def view_cart():
+    if not cart:
+        print("Cart is empty")
+        return
+
+    total = 0
+    for item, (price, qty) in cart.items():
+        total += price * qty
+
+    print("Cart---->", cart)
+    print("Total price:", total)
+
+
+def customer_menu():
+    while True:
+        print("--- Customer Menu ---")
+        print("1. Add items")
+        print("2. View cart")
+        print("3. Delete item")
+        print("4. Update quantity")
+        print("5. Exit")
+
+        ch = int(input("Enter choice: "))
+
+        if ch == 1:
+            add_items()
+        elif ch == 2:
+            view_cart()
+        elif ch == 3:
+            delete_item()
+        elif ch == 4:
+            update_item_qty()
+        elif ch == 5:
+            break 
+        else:
+            print("Invalid choice")
