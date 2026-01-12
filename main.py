63de61439860ec2083a2f79bb66d148f6726963d
vanshika114 <vs0411007@gmail.com>
2026-01-12 12:32:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import admin
 import customer
-
+import admin
+print("Welcome to Stock Smart!")
+print("1. Admin")
+print("2. Customer")
+print("3. Exit")
 while True:
-    print("=== Welcome to Stock Smart ===")
-    print("1. Admin")
-    print("2. Customer")
-    print("3. Exit")
-
-    ch = int(input("Enter your choice: "))
-
-    if ch == 1:
-        admin.admin_menu()
-    elif ch == 2:
-        customer.customer_menu()
-    elif ch == 3:
-        print("Have a Nice Day!")
-        break
-    else:
-        print("Invalid choice")
+	ch = int(input("Enter your choice: "))
+	if ch == 1:
+		admin.admin_menu()
+	elif ch == 2:
+		customer.customer_menu()
+	elif ch == 3:
+		print("Thank You!")
+		break
+	else: 
+		print("invalid choice")
